7378059c1dacd4bba5295694e8677783ffa126af
aviau <alexandre@alexandreviau.net>
2021-01-03 14:58:40 -0500

    third release attempt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="perkeepy",
-    version="0.1.2",
+    version="0.1.3",
     author="Alexandre Viau",
     author_email="alexandre@alexandreviau.net",
     description="Python utilities for Perkeep",
