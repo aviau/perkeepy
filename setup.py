1610a6cffda24884da90ac3fcc16af441552b139
aviau <alexandre@alexandreviau.net>
2021-01-03 15:03:08 -0500

    publish-to-pypi.yaml: bump gh-action-pypi-publish

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="perkeepy",
-    version="0.1.4",
+    version="0.1.5",
     author="Alexandre Viau",
     author_email="alexandre@alexandreviau.net",
     description="Python utilities for Perkeep",
