b97c7898dd039458a19d7281eda0709618ad736c
aviau <alexandre@alexandreviau.net>
2021-01-04 13:29:31 -0500

    claim -> schema

---
+++ perkeepy/schema/schema.py
@@ -0,0 +1,12 @@
+from perkeepy.blob import Blob
+
+
+class Schema:
+    """
+    A schema is a JSON-encoded blob that describes other blobs. We recognize
+    a claim by the presence of the "camliVersion" and "camliType" fields.
+    """
+
+    @classmethod
+    def from_blob(cls, blob: Blob) -> "Schema":
+        raise NotImplementedError()
