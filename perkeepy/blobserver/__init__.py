b5d3bfe4a572639069cc55ebb6de02cf9a599a22
aviau <alexandre@alexandreviau.net>
2021-01-03 21:24:35 -0500

    blobserver: initial interfaces

---
+++ perkeepy/blobserver/__init__.py
@@ -0,0 +1,2 @@
+from .interface import Storage
+from .interface import BlobEnumerator
---
+++ perkeepy/blobserver/interface.py
@@ -0,0 +1,15 @@
+from typing import Protocol
+from typing import Iterator
+from typing import Optional
+
+from perkeepy.blob import Fetcher
+from perkeepy.blob import Ref
+
+
+class BlobEnumerator(Protocol):
+    def enumerate_blobs(self, after: Optional[Ref]) -> Iterator[Ref]:
+        ...
+
+
+class Storage(Protocol, Fetcher, BlobEnumerator):
+    ...
